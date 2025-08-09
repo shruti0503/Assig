5ccbbb93aabfd8357c43cacdb95c645667744407
shrutiVishwakarma05 <shrutivishwakarma0509@gmail.com>
2025-08-09 20:33:46 +0530

    Update page.tsx

--- project/app/page.tsx
+++ project/app/page.tsx
@@ -1,9 +1,7 @@
 'use client'
-import Image from "next/image";
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
-import { Montserrat } from "next/font/google";
 import Gradient from "./components/Gradient";
 import LoadingScreen from "./components/LoadingScreen";
 
